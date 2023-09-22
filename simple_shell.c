57b0799e063a942dcbcfc8136f3271e14c55ce21
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2023-09-22 17:36:31 +0000

    updated

--- simple_shell.c
+++ simple_shell.c
@@ -80,11 +80,11 @@ void cleanup(char *line, char **command, char **paths, char *pathcommand)
 }
 
 /**
-* prompt_user - prints $ to let user know the program is
-* ready to take their input
-* prints the prompt if the shell is in interactive mode
-* Return: no return
-*/
+ * prompt_user - prints $ to let user know the program is
+ * ready to take their input
+ * prints the prompt if the shell is in interactive mode
+ * Return: no return
+ */
 void prompt_user(void)
 {
 	if ((isatty(STDIN_FILENO) == 1) && (isatty(STDOUT_FILENO) == 1))
